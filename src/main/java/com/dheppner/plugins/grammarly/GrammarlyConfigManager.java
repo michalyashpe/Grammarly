3117a9849b919d056cdda70a61140121bedf02e1
Daniel Heppner <daniel.heppner1@gmail.com>
2012-02-04 10:31:05 -0800

    Config class.

--- src/main/java/com/dheppner/plugins/grammarly/GrammarlyConfigManager.java
+++ src/main/java/com/dheppner/plugins/grammarly/GrammarlyConfigManager.java
@@ -1,10 +1,31 @@
 package com.dheppner.plugins.grammarly;
 
+import org.bukkit.configuration.file.FileConfiguration;
+
 /**
  * User: Daniel
  * Date: 2/4/12
  * Time: 12:37 AM
  */
 public class GrammarlyConfigManager {
+    final GrammarlyMain plugin;
 
+    public GrammarlyConfigManager(GrammarlyMain plugin) {
+        this.plugin = plugin;
+    }
+
+    public void loadConfiguration() {
+        plugin.getConfig().options().copyDefaults(true);
+        plugin.saveConfig();
+    }
+
+    public void reloadConfig() {
+        plugin.reloadConfig();
+    }
+
+    public void setDefaults() {
+        FileConfiguration config = plugin.getConfig();
+        config.addDefault("Fixes.Capitalization", "true");
+        config.addDefault("Fixes.Punctuation", "true");
+    }
 }
